d921225966e98b2e5c1068c853a4e599f9bb0e2a
dilaradupont <dilara.dupont@icloud.com>
2021-12-09 20:32:40 +0000

    Looked at number of labeled landmarks.

--- notes.md
+++ notes.md
@@ -1,1 +1,4 @@
-
+## Examining the dataset
+| # of Labeled Images | # of Upper GI Labels | # of Anatomical Landmark Labels | # of Z-line Labels | # of Retroflex Stomach Labels | # of Pylorus Labels |
+| - | - | - | - | - | - |
+| 10662 | 3452 | 2695 | 932 | 764 | 999 |
--- search-labels.py
+++ search-labels.py
@@ -1,2 +1,12 @@
 import pandas as pd
 
+df = pd.read_csv('file-names/image-labels.csv')
+df.drop(df.loc[df['Organ'] != 'Upper GI'].index, inplace=True)
+df.drop(df.loc[df['Classification'] != 'anatomical-landmarks'].index, inplace=True)
+df.reset_index(drop=True, inplace=True)
+print(df.shape)
+print(df['Finding'].value_counts())
+df.to_csv('file-names/filtered-names/image-labels.csv')
+
+
+

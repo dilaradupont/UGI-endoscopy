af98d4bf65e3ef4f329725ceda8510c331b91d5d
dilaradupont <dilara.dupont@icloud.com>
2021-12-11 23:14:07 +0000

    Counted labels.

--- notes.md
+++ notes.md
@@ -1,4 +1,10 @@
 ## Examining the dataset
+### Image labels
 | # of Labeled Images | # of Upper GI Labels | # of Anatomical Landmark Labels | # of Z-line Labels | # of Retroflex Stomach Labels | # of Pylorus Labels |
 | - | - | - | - | - | - |
 | 10662 | 3452 | 2695 | 932 | 764 | 999 |
+
+### Video labels
+| # of Labeled Videos | # of Upper GI Labels | # of Anatomical Landmark Labels | # of Z-line Labels | # of Retroflex Stomach Labels | # of Pyrolus Labels |
+| - | - | - | - | - | - |
+| 374 | 60 | 12 | 5 | 2 | 6 | 
--- search-labels.py
+++ search-labels.py
@@ -8,5 +8,18 @@ print(df.shape)
 print(df['Finding'].value_counts())
 df.to_csv('file-names/filtered-names/image-labels.csv')
 
-
-
+lis1 = []
+df = pd.read_csv('file-names/video-labels.csv')
+df.drop(df.loc[df['Organ'] != 'Upper GI'].index, inplace=True)
+df.reset_index(drop=True, inplace=True)
+print(df.shape)
+for i in range(df.shape[0]):
+    if (df['Classification 1'].iloc[i] != 'Anatomical landmarks') \
+        & (df['Classification 2'].iloc[i] != 'Anatomical landmarks'):
+            lis1.append(i)
+df.drop(df.index[lis1], inplace=True)
+df.reset_index(drop=True, inplace=True)
+print(df.shape)
+print(df['Finding 1'].value_counts())
+print(df['Finding 2'].value_counts())
+df.to_csv('file-names/filtered-names/video-labels.csv')

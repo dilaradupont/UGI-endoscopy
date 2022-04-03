8b77f84fbe8ed07c51af48dc579edb90c2d7464f
dilaradupont <dilara.dupont@icloud.com>
2022-04-03 12:21:01 +0100

    deleted videos.

--- split-videos.py
+++ split-videos.py
@@ -2,7 +2,7 @@ import cv2
 # from moviepy.editor import *
 
 STARTING_FRAME = 393
-IN_PATH = './videos/OGD_1.mov'
+IN_PATH = '/home/dilara/UGI-endoscopy/videos/OGD_1.mov'
 OUT_PATH = '/home/dilara/UGI-endoscopy/video-split-images/video1-split'
 
 # ## Finding out the FPS - source: https://www.geeksforgeeks.org/moviepy-fps-of-video-file-clip/?ref=lbp
@@ -10,17 +10,17 @@ OUT_PATH = '/home/dilara/UGI-endoscopy/video-split-images/video1-split'
 # rate = clip.fps
 # print("FPS : " + str(rate)) 
 
-count = STARTING_FRAME
-## Splitting the video into frames - source: https://techtutorialsx.com/2021/04/29/python-opencv-splitting-video-frames/
-video = cv2.VideoCapture(IN_PATH)
-while(True):
-    success, frame = video.read()
-    if success:
-        pass
-    else:
-        break
-    count +=1
-    if count % 10 == 0:
-        cv2.imwrite(f'./video-split-images/video1-split/frame_{count}.jpg', frame)
+# count = STARTING_FRAME
+# ## Splitting the video into frames - source: https://techtutorialsx.com/2021/04/29/python-opencv-splitting-video-frames/
+# video = cv2.VideoCapture(IN_PATH)
+# while(True):
+#     success, frame = video.read()
+#     if success:
+#         pass
+#     else:
+#         break
+#     count +=1
+#     if count % 10 == 0:
+#         cv2.imwrite(f'./video-split-images/video1-split/frame_{count}.jpg', frame)
 
-video.release()
+# video.release()

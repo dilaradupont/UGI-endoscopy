c3d13890270a96da7bb479c2f8cdb7909ca560f6
dilaradupont <dilara.dupont@icloud.com>
2022-04-03 09:45:26 +0100

    Added videos.

--- split-videos.py
+++ split-videos.py
@@ -1,13 +1,15 @@
 import cv2
 from moviepy.editor import *
-  
+
+STARTING_FRAME = 0
+
 ## Finding out the FPS - source: https://www.geeksforgeeks.org/moviepy-fps-of-video-file-clip/?ref=lbp
 clip = VideoFileClip('/Users/dilaradupont/Desktop/UCL/Year 3/UGI-endoscopy/OGD_1.mov').subclip(0, 5)
 rate = clip.fps
 print("FPS : " + str(rate)) 
 
+count = STARTING_FRAME
 ## Splitting the video into frames - source: https://techtutorialsx.com/2021/04/29/python-opencv-splitting-video-frames/
-count = 0
 video = cv2.VideoCapture('/Users/dilaradupont/Desktop/UCL/Year 3/UGI-endoscopy/OGD_1.mov')
 while(True):
     success, frame = video.read()
@@ -15,7 +17,7 @@ while(True):
         pass
     else:
         break
-    count = count+1
+    count +=1
     if count % 10 == 0:
         cv2.imwrite(f'/Users/dilaradupont/Desktop/UCL/Year 3/UGI-endoscopy/video-split/frame_{count}.jpg', frame)
 
